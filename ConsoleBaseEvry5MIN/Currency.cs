5270bc2d521957e6c7821dc74fffcd33ed8c7a0f
ТолстовА <@204_9>
2018-12-15 11:28:11 +0600

    1

---
+++ ConsoleBaseEvry5MIN/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBaseEvry5MIN
+{
+    class Currency
+    {
+        public string title { get; set; }
+        public DateTime PubDate { get; set; }
+        public double Description { get; set; }
+    }
+}
--- ConsoleBaseEvry5MIN/Program.cs
+++ ConsoleBaseEvry5MIN/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Xml.Linq;
 
 namespace ConsoleBaseEvry5MIN
 {
@@ -13,7 +15,27 @@ namespace ConsoleBaseEvry5MIN
         }
         public static void getCurrency()
         {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
 
+            try
+            {
+                List<Currency> сurrencies = new List<Currency>();
+                XDocument xdoc = XDocument.Load(ConnectionString);
+
+                foreach (XElement item in xdoc.Element("rss").Element("channel").Elements("item"))
+                {
+                    Currency cur = new Currency();
+                    cur.title = item.Element("title").Value;
+                    cur.PubDate = Convert.ToDateTime(item.Element("pubDate").Value);
+                    cur.Description = Convert.ToDouble(item.Element("description").Value.Replace('.', ','));
+
+                    сurrencies.Add(cur);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
